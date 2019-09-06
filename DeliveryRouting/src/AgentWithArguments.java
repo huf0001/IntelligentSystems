f2629e4555d9e9f79dceb5222dda887521813c95
liamcgd <liam_cg_dunstan@yahoo.com.au>
2019-09-06 10:48:14 +1000

    add basic intellij project

---
+++ DeliveryRouting/src/AgentWithArguments.java
@@ -0,0 +1,10 @@
+import jade.core.Agent;
+
+public class AgentWithArguments extends Agent {
+    protected void setup() {
+        Object[] args = getArguments();
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
