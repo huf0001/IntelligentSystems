e85ede0b85902cdfe98b237d8dd305b113c158e7
CyrusEdgren <1010606991@student.swin.edu.au>
2019-09-12 21:41:23 +1000

    Formatted and commented a bit.

--- DeliveryRouting/src/Depot.java
+++ DeliveryRouting/src/Depot.java
@@ -22,19 +22,25 @@ public class Depot extends Agent{
     protected void setup(){
         CyclicBehaviour listenForRouteQueries = new CyclicBehaviour(this) {
             public void action() {
+                // Match a request for a route
                 MessageTemplate mt = MessageTemplate.and(MessageTemplate.MatchPerformative(ACLMessage.REQUEST),
                         MessageTemplate.MatchConversationId("Route_Request"));
                 ACLMessage request = receive(mt);
+
                 if (request != null) {
                     ACLMessage reply = request.createReply();
                     reply.setPerformative(ACLMessage.INFORM);
+
+                    // Send the route according to the AID of the truck
                     reply.setContent(routes.get(request.getSender()).toString());
+
                     send(reply);
                 } else {
                     block();
                 }
             }
         };
+        addBehaviour(listenForRouteQueries);
     }
 
     public void GetConstraints() {
