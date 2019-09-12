df58b6a203e1f8330e356f2194e4deafbe3977bc
CyrusEdgren <1010606991@student.swin.edu.au>
2019-09-12 13:08:24 +1000

    Added Depot functionallity to ask all trucks at depot for their weight constraints and to store them.

--- DeliveryRouting/src/Depot.java
+++ DeliveryRouting/src/Depot.java
@@ -1,15 +1,91 @@
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Depot extends Agent{
     private World world;
     private List<Parcel> parcels;
-    private List<Truck> trucksAtDepot;
+    private List<AID> trucksAtDepot;
+    private Map<AID, Float> truckCapacity = new HashMap<AID, Float>();
     private List<List<Road>> routes;
     private List<Node> unroutedNodes;
     private List<Node> routedNodes;
 
-    public void GetConstraint(Truck truck) {
+
+    protected void setup(){
+        CyclicBehaviour listeningBehaviour = new CyclicBehaviour(this) {
+            public void action() {
+
+            }
+        };
+    }
+
+    public void GetConstraint(AID truck) {
+        Behaviour getConstraint = new Behaviour(this) {
+            private int truckAmount = 0;
+            private int truckResponsesReceived = 0;
+            private int step = 0;
+            private MessageTemplate mt;
+
+            public void action() {
+                switch (step) {
+                    case 0:
+                        ACLMessage request = new ACLMessage(ACLMessage.REQUEST);
+
+                        // Ask all trucks at depot
+                        for (AID truck : trucksAtDepot) {
+                            request.addReceiver(truck);
+                            truckAmount++;
+                        }
+
+                        // Setup request values
+                        request.setContent("Capacity");
+                        request.setConversationId("Constraint_Request");
+                        request.setReplyWith("request" + System.currentTimeMillis()); // Unique ID
+
+                        // Send request
+                        send(request);
+
+                        // Setup template to receive responses
+                        mt = MessageTemplate.and(MessageTemplate.MatchConversationId("Constraint_Request"),
+                                MessageTemplate.MatchInReplyTo(request.getReplyWith()));
+
+                        step = 1;
+                        break;
+                    case 1:
+                        ACLMessage reply = receive(mt);
+                        // Wait for replies and store their content
+                        if (reply != null) {
+                            if (reply.getPerformative() == ACLMessage.INFORM) {
+                                if (truckCapacity.containsKey(reply.getSender())) {
+                                    truckCapacity.replace(reply.getSender(), Float.parseFloat(reply.getContent()));
+                                } else {
+                                    truckCapacity.put(reply.getSender(), Float.parseFloat(reply.getContent()));
+                                }
+                            }
+                            truckResponsesReceived++;
+                        } else {
+                            block();
+                        }
+
+                        if (truckResponsesReceived >= truckAmount) {
+                            step = 2;
+                        }
+                        break;
+                }
+            }
+
+            public boolean done() {
+                return step == 2;
+            }
+        };
         throw new UnsupportedOperationException("Not implemented");
     }
 
@@ -21,7 +97,7 @@ public class Depot extends Agent{
         throw new UnsupportedOperationException("Not implemented");
     }
 
-    public void GiveRoute(Truck){
+    public void GiveRoute(AID truck){
         throw new UnsupportedOperationException("Not implemented");
     }
 }
