6f059baeeba2e0a6fb24bfcf6a267b1aa7473d6a
CyrusEdgren <101606991@student.swin.edu.au>
2019-09-25 13:48:15 +1000

    Added ParcelGenerator class.

--- DeliveryRouting/src/Parcel.java
+++ DeliveryRouting/src/Parcel.java
@@ -1,2 +1,11 @@
 public class Parcel {
+    int id;
+    int weight;
+    Node destination;
+
+    Parcel(int aId, int aWeight){
+        id = aId;
+        weight = aWeight;
+    }
+
 }
---
+++ DeliveryRouting/src/ParcelGenerator.java
@@ -0,0 +1,39 @@
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ParcelGenerator {
+    private static final int NUM = 30;
+    private Parcel[] parcels = new Parcel[NUM];
+
+    public static void main(String[] args) {
+        ParcelGenerator generator = new ParcelGenerator();
+        generator.CreateParcels(NUM);
+        generator.DumpToJson();
+    }
+
+    private void CreateParcels(int num) {
+        Random rand = new Random();
+        for (int i = 0; i < num; i++) {
+            Parcel parcel = new Parcel(i, rand.nextInt(9) + 1);
+            parcels[i] = parcel;
+        }
+    }
+
+    private void DumpToJson() {
+        if (parcels.length != 0){
+            Gson gson = new Gson();
+            try{
+                FileWriter writer = new FileWriter("Parcels.json");
+                writer.write(gson.toJson(parcels));
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+    }
+}
