20d109adcb66bfe9a09cc405116a3806d6f82563
Steven-Hart <stevenhart11@gmail.com>
2019-09-29 19:50:08 +1000

    Base java2d class

--- DeliveryRouting/src/World.java
+++ DeliveryRouting/src/World.java
@@ -1,2 +1,12 @@
-public class World {
+import javax.swing.JFrame;
+
+public class World extends JFrame {
+    public World() {
+        setTitle("Test");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 }
