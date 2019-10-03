06c2d2ec264b353716e0030e3323fd071a81aec3
Steven-Hart <stevenhart11@gmail.com>
2019-10-03 23:33:24 +1000

    Fixed Merge in Node.java

--- DeliveryRouting/src/Node.java
+++ DeliveryRouting/src/Node.java
@@ -3,6 +3,30 @@ import java.util.List;
 
 public class Node
 {
+    int id, weight;
+    Vector2 position;
+
+    static class WorldPoint{
+        int x;
+        int y;
+
+        WorldPoint(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    Node(int value)
+    {
+        this.id = value;
+    }
+
+    Node(int value, Vector2 position)
+    {
+        this.id = value;
+        this.position = position;
+    }
+
     //Private Fields---------------------------------------------------------------------------------------------------------------------------------
 
     private List<Parcel> parcels = new ArrayList<Parcel>();
